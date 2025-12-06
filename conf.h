44a03928d18874b22f7d8da51c316eeabb6a8877
Juan Francisco Barrios <juanfranciscobarrios49@gmail.com>
2025-12-05 21:19:18 -0300

    Update conf.h

--- conf.h
+++ conf.h
@@ -1,54 +1,67 @@
 #ifndef CONF_H
-#define CONF_H
-
+#define CONF_H 1
 #include <inttypes.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 #include "avr_api.h"
 
 typedef enum {
     INICIO = 0,
-    MANUAL_STOP,
-    MANUAL_SUBIENDO,
-    MANUAL_BAJANDO,
-    AUTO_STOP,
-    AUTO_SUBIENDO,
-    AUTO_BAJANDO
+    ESPERA = 1,
+    SUBIR  = 2,
+    BAJAR  = 3
 } estado_t;
 
-typedef struct {
-    uint8_t up;
-    uint8_t down;
-} accion_t;
+#define UMBRAL_NOCHE  300
+#define UMBRAL_DIA    700
 
-// prototipos
+//fines de carrera
+#define SENS_PORT     avr_GPIO_C
+#define S3_PIN        avr_GPIO_PIN_0
+#define S2_PIN        avr_GPIO_PIN_1
+#define S1_PIN        avr_GPIO_PIN_2 
+
+#define LEER_S3       avr_GPIOC_IN_0
+#define LEER_S2       avr_GPIOC_IN_1
+#define LEER_S1       avr_GPIOC_IN_2
+
+//botones (subir y bajar) y modo
+#define CTRL_PORT     avr_GPIO_B
+#define MODO_PIN      avr_GPIO_PIN_0
+#define SUBIR_PIN     avr_GPIO_PIN_1
+#define BAJAR_PIN     avr_GPIO_PIN_2
+
+#define LEER_MODO_AUTO avr_GPIOB_IN_0
+#define LEER_BTN_SUB   avr_GPIOB_IN_1
+#define LEER_BTN_BAJ   avr_GPIOB_IN_2
+
+//L298 para motor
+#define MOTOR_PORT    avr_GPIO_D
+#define IN1_PIN       avr_GPIO_PIN_0
+#define IN2_PIN       avr_GPIO_PIN_1
+#define ENA_PIN       avr_GPIO_PIN_2
+
+#define MOTOR_IN1     avr_GPIOD_OUT_0
+#define MOTOR_IN2     avr_GPIOD_OUT_1
+#define MOTOR_ENA     avr_GPIOD_OUT_2
+
+//LDR
+#define CANAL_LUZ     avr_ADC_canal0 
+
+extern int valor_luz;
+
+//prototipos de funciones
 void init_mcu(void);
-accion_t driver_leer(void);
-void driver_ejecutar(accion_t ac);
+void leer_sensores_luz(void);
+void motor_stop(void);
+void motor_subir(void);
+void motor_bajar(void);
 
-// estados
+//prototipos de estados
 estado_t e_inicio(void);
-estado_t e_manual_stop(void);
-estado_t e_manual_subiendo(void);
-estado_t e_manual_bajando(void);
-estado_t e_auto_stop(void);
-estado_t e_auto_subiendo(void);
-estado_t e_auto_bajando(void);
-
-// pines
-#define PIN_LDR   0     // ADC0, PA0
-#define PIN_MODO avr_GPIOB_IN_0
-#define PIN_SUB avr_GPIOB_IN_1
-#define PIN_BAJ avr_GPIOB_IN_2
-
-#define PIN_S1 avr_GPIOC_IN_2  
-#define PIN_S2 avr_GPIOC_IN_1  
-#define PIN_S3avr_GPIOC_IN_0  
-
-#define MOTOR_PORT avr_GPIO_D
-#define MOTOR_UP avr_GPIOD_OUT_0
-#define MOTOR_DOWN avr_GPIOD_OUT_1
-#define MOTOR_EN avr_GPIOD_OUT_2
+estado_t e_espera(void);
+estado_t e_subir(void);
+estado_t e_bajar(void);
 
 #endif
-
